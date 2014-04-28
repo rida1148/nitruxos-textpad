15c866ba95b86155ea0c83f541af6e8f9972ea68
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2014-04-28 10:03:15 -0300

    Declare the methods as slots using the "public slots"
    In order to enable the calling of Qt class methods from QML code, the
    methods must be declared as public slots or with the Q_INVOKABLE macro.
    There is no difference how you declare a slot, but if you have many slots
    should better encapsulate these slots using the "public slots" declaration.

--- fileIO.h
+++ fileIO.h
@@ -7,9 +7,9 @@ class FileIO : public QObject
     Q_OBJECT
 
     QString filePath;
-public:
+public slots:
 
-    Q_INVOKABLE void setPath(const QString &fileURL);
-    Q_INVOKABLE QString read();
-    Q_INVOKABLE bool write(const QString &data);
+    void setPath(const QString &fileURL);
+    QString read();
+    bool write(const QString &data);
 };
